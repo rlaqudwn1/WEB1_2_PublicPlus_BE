eae55552863b21b830b80e25064bbb0f80f9944d
jinwoo <dlwlsdn2628@hanmail.net>
2024-11-21 16:15:14 +0900

    .

---
+++ src/main/java/backend/dev/facility/repository/FacilityRepository.java
@@ -0,0 +1,7 @@
+package backend.dev.facility.repository;
+
+import backend.dev.facility.entity.Facility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacilityRepository extends JpaRepository<Facility, Long> {
+}
