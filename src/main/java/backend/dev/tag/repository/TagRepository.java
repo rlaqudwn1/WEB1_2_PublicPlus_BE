c607ca9ffab7f39e8ecab96e55d40f9c90bcd590
jinwoo <dlwlsdn2628@hanmail.net>
2024-11-22 11:09:29 +0900

    .

--- src/main/java/backend/dev/tag/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.dev.tag.repository;
-
-import backend.dev.tag.entity.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
