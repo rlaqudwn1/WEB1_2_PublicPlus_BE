369f865888831816cfd110108485f18647b072c5
rlaqudwn1 <rlaqudwn123456@gmail.com>
2024-11-25 15:39:12 +0900

    Fix: "Review 연동 및 Test 버그 수정"

--- src/main/java/backend/dev/facility/dto/facility/FacilityUpdateDTO.java
+++ src/main/java/backend/dev/facility/dto/facility/FacilityUpdateDTO.java
@@ -1,7 +1,6 @@
 package backend.dev.facility.dto.facility;
 
 import backend.dev.facility.dto.FacilityBaseDTO;
-import backend.dev.facility.entity.Point;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -12,5 +11,6 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 public class FacilityUpdateDTO extends FacilityBaseDTO {
 
-    private Point location; // 좌표 (변경 가능)
+    private Float latitude;
+    private Float longitude;
 }
