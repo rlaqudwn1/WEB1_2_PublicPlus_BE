ab4c8eb7cd5b5b9908c5ce21add2ea12e1612425
rlaqudwn1 <rlaqudwn123456@gmail.com>
2024-12-06 23:47:40 +0900

    FeatANDBugFIX : 조회수 기능 추가(중복방지 1분 과 로그인 필수) 좋아요 횟수랑 좋아요 여부 확인에 대한 버그 픽스

--- src/main/java/backend/dev/activity/repository/ActivityRepository.java
+++ src/main/java/backend/dev/activity/repository/ActivityRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    boolean existsActivityByActivity_IdAndUser_Id(Long activityId, Long userId);
 
 }
